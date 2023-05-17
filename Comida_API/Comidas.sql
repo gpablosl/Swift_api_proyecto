27f66f1b278565471ced7a76db6f7246beee4302
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-05-17 15:45:28 -0700

    Add files via upload

--- Comida_API/Comidas.sql
+++ Comida_API/Comidas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 10-05-2023 a las 22:01:37
+-- Tiempo de generación: 17-05-2023 a las 22:44:44
 -- Versión del servidor: 5.7.39
 -- Versión de PHP: 7.4.33
 
@@ -35,24 +35,26 @@ CREATE TABLE `Comidas` (
   `pais` varchar(255) NOT NULL,
   `saludable` varchar(255) NOT NULL,
   `rapida` varchar(255) NOT NULL,
-  `imagen` varchar(255) NOT NULL
+  `imagen` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `Comidas`
 --
 
-INSERT INTO `Comidas` (`id`, `nombre`, `tipo`, `costo`, `pais`, `saludable`, `rapida`, `imagen`) VALUES
-(1, 'Pizza', 'Carne triple', '$355', 'Italia', 'No', 'Si', 'pizza'),
-(2, 'Hamburguesa', 'Triple bbq', '$140 ', 'Alemania', 'No', 'Si', 'hamburguesa'),
-(3, 'Spaghetti', 'Rojo', '$180', 'Italia', 'No', 'No', 'imagen'),
-(4, 'Tacos', 'bistec', '$20', 'México', 'No', 'No', 'imagen'),
-(5, 'Hamburguesa', 'Doble', '$150', 'Alemania', 'No', 'No', 'Imagen'),
-(6, 'Pizza', 'Pepperoni', '$220', 'Italia', 'No', 'Si', 'Imagen'),
-(7, 'Sushi', 'Nigiri', '$200', 'Japón', 'Si', 'No', 'Imagen'),
-(8, 'Mole', 'Poblano', '$150', 'México', 'Si', 'No', 'Imagen'),
-(9, 'HotDog', 'Salchipapa', '$80', 'Estados Unidos', 'No', 'Si', 'Imagen'),
-(10, 'Torta', 'Ahogada', '$120', 'México', 'No', 'Si', 'Imagen');
+INSERT INTO `Comidas` (`id`, `nombre`, `tipo`, `costo`, `pais`, `saludable`, `rapida`, `imagen`, `created_at`, `updated_at`) VALUES
+(1, 'Pizza', 'Carne triple', '$355', 'Italia', 'No', 'Si', 'SW8SQFRCNgmzsIC2NoZeM2KdOaDtzbOeGTpw3P3D.jpg', '2023-05-17 20:42:38', '2023-05-18 03:55:02'),
+(2, 'Hamburguesa', 'Triple bbq', '$140', 'Alemania', 'No', 'Si', 'zZ5mDE3fQ4eBIHaTEbEqCM0T4VpKZP2jqzEepZaU.jpg', '2023-05-17 20:42:38', '2023-05-18 03:55:13'),
+(3, 'Spaghetti', 'Rojo', '$180', 'Italia', 'No', 'No', 'QJQIx5mOwzHo0AkaQTFkjcUy8yBWlqDGoq9wNiUA.jpg', '2023-05-17 20:42:38', '2023-05-18 03:55:22'),
+(4, 'Tacos', 'bistec', '$20', 'México', 'No', 'No', '2YcwzZ4nxKNc4mr5UaT5YihkyvTI4nWvpGVVnk1u.jpg', '2023-05-17 20:42:38', '2023-05-18 03:56:04'),
+(5, 'Hamburguesa', 'Doble', '$150', 'Alemania', 'No', 'No', '24I9yABEGGodxFOJOrxymNJfYuxbYVRooERSlP9S.webp', '2023-05-17 20:42:38', '2023-05-18 03:56:12'),
+(6, 'Pizza', 'Pepperoni', '$220', 'Italia', 'No', 'Si', 'IwmAJc4rfZc5CwDJHnq1kRbKBIb7yNYxDEX8Nyzb.jpg', '2023-05-17 20:42:38', '2023-05-18 03:56:18'),
+(7, 'Sushi', 'Nigiri', '$200', 'Japón', 'Si', 'No', 'BTJsEm2hc7dgQyGcSopu87vxejRCrXQ21e08sy2p.jpg', '2023-05-17 20:42:38', '2023-05-18 03:56:33'),
+(8, 'Mole', 'Poblano', '$150', 'México', 'Si', 'No', '4E9taqjGUC6CCiR1fLmokw3wYv4s8uaZ2Y4CBKHc.jpg', '2023-05-17 20:42:38', '2023-05-18 03:56:42'),
+(9, 'HotDog', 'Salchipapa', '$80', 'Estados Unidos', 'No', 'Si', 'Vpsc7X3vkKAucKbE8bJv7bJ0yK7wz1wT02AVr0Sp.jpg', '2023-05-17 20:42:38', '2023-05-18 03:56:52'),
+(10, 'Torta', 'Ahogada', '$120', 'México', 'No', 'Si', 'WVCCj200FNZ8UFDUxrY7mDSuEZYFNWwmFjvAtrLw.jpg', '2023-05-17 20:42:38', '2023-05-18 03:57:20');
 
 --
 -- Índices para tablas volcadas
@@ -72,7 +74,7 @@ ALTER TABLE `Comidas`
 -- AUTO_INCREMENT de la tabla `Comidas`
 --
 ALTER TABLE `Comidas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
